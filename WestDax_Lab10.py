47e2d5a1ad9290af2ab0181a17eb2a43f1be6cee
DaxWest <awest49@uwo.ca>
2024-11-19 13:55:26 -0500

    initialized function for part 3

--- WestDax_Lab10.py
+++ WestDax_Lab10.py
@@ -6,3 +6,7 @@ import matplotlib.pyplot as plt
 def make_initialcond(x, sigma_0, k_0):
     a_x = np.exp(-(x**2)/(2*(sigma_0**2))) * np.cos(k_0*x)
     return a_x
+
+#part 3
+def spectral_radius(A):
+    #needs to use np.linalg.eig
