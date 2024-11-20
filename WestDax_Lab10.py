02c8b33b55183ae6c3fa42ce14d07ce661b15a8b
DaxWest <awest49@uwo.ca>
2024-11-20 15:07:21 -0500

    producing the tridiagonal matrix

--- WestDax_Lab10.py
+++ WestDax_Lab10.py
@@ -3,13 +3,21 @@ import matplotlib.pyplot as plt
 
 #part 1
 def make_tridiagonal(N, b, d, a):
-    #need to make and NxN matrix
+    matrix = d*np.identity(N)+b*np.diagflat(np.ones(N-1),1)+a*np.diagflat(np.ones(N-1),-1)
+    return matrix
+N = 5
+b = 3
+d = 1
+a = 5
 
-#part 2
-def make_initialcond(x, sigma_0, k_0):
-    a_x = np.exp(-(x**2)/(2*(sigma_0**2))) * np.cos(k_0*x)
-    return a_x
+A = make_tridiagonal(N, b, d, a)
+print(A)
 
-#part 3
-def spectral_radius(A):
-    #needs to use np.linalg.eig
+# #part 2
+# def make_initialcond(x, sigma_0, k_0):
+#     a_x = np.exp(-(x**2)/(2*(sigma_0**2))) * np.cos(k_0*x)
+#     return a_x
+#
+# #part 3
+# def spectral_radius(A):
+#     #needs to use np.linalg.eig
