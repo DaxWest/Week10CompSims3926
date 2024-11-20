3762296983fe0c8a8502bce34fa45b224a6ec28a
DaxWest <awest49@uwo.ca>
2024-11-20 15:17:18 -0500

    produced x and a_x for plotting in part 2

--- WestDax_Lab10.py
+++ WestDax_Lab10.py
@@ -13,11 +13,19 @@ a = 5
 A = make_tridiagonal(N, b, d, a)
 print(A)
 
-# #part 2
-# def make_initialcond(x, sigma_0, k_0):
-#     a_x = np.exp(-(x**2)/(2*(sigma_0**2))) * np.cos(k_0*x)
-#     return a_x
-#
+#part 2
+def make_initialcond(x, sigma_0, k_0):
+    return np.exp(-(x**2)/(2*(sigma_0**2))) * np.cos(k_0*x)
+
+N_space = 300
+L = 5
+sigma_0 = 0.2
+k_0 = 35
+x = np.linspace(-L/2, L/2, N_space)
+
+a_x = make_initialcond(x, sigma_0, k_0)
+print(a_x)
+
 # #part 3
 # def spectral_radius(A):
 #     #needs to use np.linalg.eig
